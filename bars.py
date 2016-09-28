ddd0b2ae51cf85c06aebec020543e423f4aabe1f
Юрий Атрощенко <atr.yury@gmail.com>
2016-09-28 15:25:36 +0300

    Исправления замечаний учителя

--- bars.py
+++ bars.py
@@ -3,138 +3,111 @@ import math
 
 
 def load_data(filepath):
-    f = open(filepath, 'r')
-    content = f.read()
-    f.close()
-    return json.loads(content)
+    """
+        @param filepath Путь к файлу в формате json
+        @return Данные файла
+    """
+    try:
+        with open(filepath, 'r', encoding='utf-8') as f:
+            return json.load(f)
+    except FileNotFoundError:
+        print('File not found: "{}"'.format(filepath))
+    except PermissionError:
+        print('Access to file denied: "{}"'.format(filepath))
+    except Exception as e:
+        print('Error: {}'.format(e))
 
-def get_biggest_bar(data):
+def get_content(bar):
+    """
+        @param bar Данные бара
+        @return Контент
+    """
+    content = bar['Cells']['Name'] + '\n'
+    content += 'Количество мест: %s \n' % bar['Cells']['SeatsCount']
+    content += '{}, {}, {} \n'.format(
+        bar['Cells']['AdmArea'],
+        bar['Cells']['District'],
+        bar['Cells']['Address'])
+    content += 'Контакты:\n'
+    for phone in bar['Cells']['PublicPhone']:
+        content += '  - ' + phone['PublicPhone'] + '\n'
+    content += '\n'
+    return content
+
+def get_bar(data, type_bar=None):
     """
         @param data Список дынных
-        @return Самый большой бар из списка
+        @param type_bar Тип искомого бара
+        @return Самый большой или маленький бар из списка
     """
-    biggest = None
+    if type_bar not in ['biggest', 'smallest']:
+        print('Не указан тип бара!')
+        return None
+    seeking_bar = data.pop(0)
     for bar in data:
-        if biggest is None:
-            biggest = bar
-            continue
-        if bar['Cells']['SeatsCount'] > biggest['Cells']['SeatsCount']:
-            biggest = bar
-    return biggest
+        if type_bar is 'biggest':
+            if bar['Cells']['SeatsCount'] > seeking_bar['Cells']['SeatsCount']:
+                seeking_bar = bar
+        if type_bar is 'smallest':
+            if bar['Cells']['SeatsCount'] < seeking_bar['Cells']['SeatsCount']:
+                seeking_bar = bar
+    return get_content(seeking_bar)
 
-def get_smallest_bar(data):
+def get_gps_coordinates():
     """
-        @param data Список дынных
-        @return Самый маленький бар из списка
+        @return gps-координаты
     """
-    smallest = None
-    for bar in data:
-        if smallest is None:
-            smallest = bar
-            continue
-        if bar['Cells']['SeatsCount'] < smallest['Cells']['SeatsCount']:
-            smallest = bar
-    return smallest
+    while True:
+        try:
+            print('Введите текущие gps-координаты (долгота, широта) через запятую:')
+            longitude, latitude = map(float, input('> ').split(','))
+            break
+        except Exception as e:
+            print('Ошибка: не верно введены gps-координаты! Введите число. (Пример: "55.12345, 37.12345")')
+    return longitude, latitude
 
 
-def get_closest_bar(data, longitude, latitude):
+def get_distance(bar, longitude, latitude):
+    """
+        @param bar Список дынных
+        @param longitude Долгота
+        @param latitude Широта
+        @return дистанция по gps-координатам
+    """
+    longitude_bar = bar['Cells']['geoData']['coordinates'][1]
+    latitude_bar = bar['Cells']['geoData']['coordinates'][0]
+    return abs(math.sqrt((longitude - longitude_bar)**2 + (latitude - latitude_bar)**2))
+
+def get_closest_bar(data, longitude, latitude, show_count=1):
     """
         @param data Список дынных
         @param longitude Долгота
         @param latitude Широта
+        @return Контен
     """
     for index, bar in enumerate(data):
-        # Долгота бара
-        x = bar['Cells']['geoData']['coordinates'][1]
-        # Широта бара
-        y = bar['Cells']['geoData']['coordinates'][0]
         # Расчёт растояния до ближайшей geo-точки
-        distance = abs(math.sqrt((longitude - x)**2 + (latitude - y)**2))
+        distance = get_distance(bar, longitude, latitude)
         data[index]['Distance'] = round(distance, 15)
     # Отсортируем список
     data.sort(key=lambda x: x['Distance'])
-    return data
+    content = ''
+    for bar in data[:show_count]:
+        content += get_content(bar)
+    return content
 
 def main():
-    content = ''
-
-    # Получим данные
-    data = load_data('bars.json')
-
-    # Самый большой бар
-    count_biggest_bar = get_biggest_bar(data)
-    content += 'Самый большой бар:\n'
-    content += count_biggest_bar['Cells']['Name'] + '\n'
-    content += 'Количество мест: %s \n' % count_biggest_bar['Cells']['SeatsCount']
-    content += '{}, {}, {} \n'.format(
-        count_biggest_bar['Cells']['AdmArea'],
-        count_biggest_bar['Cells']['District'],
-        count_biggest_bar['Cells']['Address'])
-    content += 'Контакты:\n'
-    for phone in count_biggest_bar['Cells']['PublicPhone']:
-        content += '  - ' + phone['PublicPhone'] + '\n'
-    content += '\n'
-
-    # Самый маленький бар
-    count_smallest_bar = get_smallest_bar(data)
-    content += 'Самый маленький бар:\n'
-    content += count_smallest_bar['Cells']['Name'] + '\n'
-    content += 'Количество мест: %s \n' % count_smallest_bar['Cells']['SeatsCount']
-    content += '{}, {}, {} \n'.format(
-        count_smallest_bar['Cells']['AdmArea'],
-        count_smallest_bar['Cells']['District'],
-        count_smallest_bar['Cells']['Address'])
-    content += 'Контакты:\n'
-    for phone in count_smallest_bar['Cells']['PublicPhone']:
-        content += '  - ' + phone['PublicPhone'] + '\n'
-    content += '\n'
-
-    # Проинфирмируем
-    print(content)
-
-
-    # Ближайший бар по координатам
-    content = ''
-
-    # Вод данных с клавиатуры
-    while True:
-        try:
-            longitude = float(input('Введите долготу: '))
-            break
-        except Exception as e:
-            print('Ошибка: не верно введена долгота! Введите число. (Пример: 55.750623563675717)')
-    while True:
-        try:
-            latitude = float(input('Введите широту: '))
-            break
-        except Exception as e:
-            print('Ошибка: не верно введена широта! Введите число. (Пример: 37.582406638647662)')
-
-
-    # Расчёт
-    closest_bars = get_closest_bar(data, longitude, latitude)
-
-    # Количество ближайших баров
-    show_closest_bars = 1
-
-    # Контент
-    for index, bar in enumerate(closest_bars):
-        content += bar['Cells']['Name'] + '\n'
-        content += 'Количество мест: %s \n' % bar['Cells']['SeatsCount']
-        content += '{}, {}, {} \n'.format(
-            bar['Cells']['AdmArea'],
-            bar['Cells']['District'],
-            bar['Cells']['Address'])
-        content += 'Контакты:\n'
-        for phone in bar['Cells']['PublicPhone']:
-            content += '  - ' + phone['PublicPhone'] + '\n'
-        content += '\n'
-        if index + 1 == show_closest_bars:
-            break
-
-    # Проинфирмируем
-    print(content)
-
+    print('Введите путь к файлу со список московских баров (в формате json):')
+    data = load_data(input('> '))
+    if data:
+        # Получение gps-координаты с клавиатуры
+        longitude, latitude = get_gps_coordinates()
+        print('-'*100)
+        print('Cамый большой бар:\n', get_bar(data, type_bar='biggest'))
+        print('-'*100)
+        print('Cамый маленький бар:\n', get_bar(data, type_bar='smallest'))
+        print('-'*100)
+        print('Cамые ближайшие бары:\n', get_closest_bar(data, longitude, latitude, 1))
 
 if __name__ == '__main__':
     try:
